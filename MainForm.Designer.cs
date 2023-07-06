1597f220f5a39767b738d470356f228ba567b84e
Александр Русяев <rusyaev1985@mail.ru>
2023-07-06 18:13:56 +0500

    Добавил меню и лайблы для отображения кол-во ходов на основной форме. Отредактировал положение боксов с картинками и размер основной формы игры.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -38,6 +38,15 @@
             pictureBox6 = new PictureBox();
             pictureBox7 = new PictureBox();
             pictureBox8 = new PictureBox();
+            menuStrip = new MenuStrip();
+            менюToolStripMenuItem = new ToolStripMenuItem();
+            начатьСначалаToolStripMenuItem = new ToolStripMenuItem();
+            выходToolStripMenuItem = new ToolStripMenuItem();
+            информацияToolStripMenuItem = new ToolStripMenuItem();
+            правилаИгрыToolStripMenuItem = new ToolStripMenuItem();
+            оToolStripMenuItem = new ToolStripMenuItem();
+            NumberMovesLabelText = new Label();
+            NumberMovesLabel = new Label();
             ((System.ComponentModel.ISupportInitialize)frogLeftPictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
@@ -47,12 +56,13 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            menuStrip.SuspendLayout();
             SuspendLayout();
             // 
             // frogLeftPictureBox1
             // 
             frogLeftPictureBox1.Image = (Image)resources.GetObject("frogLeftPictureBox1.Image");
-            frogLeftPictureBox1.Location = new Point(0, 0);
+            frogLeftPictureBox1.Location = new Point(0, 27);
             frogLeftPictureBox1.Margin = new Padding(0);
             frogLeftPictureBox1.Name = "frogLeftPictureBox1";
             frogLeftPictureBox1.Size = new Size(104, 110);
@@ -63,7 +73,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(104, 0);
+            pictureBox1.Location = new Point(104, 27);
             pictureBox1.Margin = new Padding(0);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(104, 110);
@@ -74,7 +84,7 @@
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(208, 0);
+            pictureBox2.Location = new Point(208, 27);
             pictureBox2.Margin = new Padding(0);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(104, 110);
@@ -85,7 +95,7 @@
             // pictureBox3
             // 
             pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(312, 0);
+            pictureBox3.Location = new Point(312, 27);
             pictureBox3.Margin = new Padding(0);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(104, 110);
@@ -96,7 +106,7 @@
             // pictureBox4
             // 
             pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
-            pictureBox4.Location = new Point(416, 0);
+            pictureBox4.Location = new Point(416, 27);
             pictureBox4.Margin = new Padding(0);
             pictureBox4.Name = "pictureBox4";
             pictureBox4.Size = new Size(104, 110);
@@ -107,7 +117,7 @@
             // pictureBox5
             // 
             pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
-            pictureBox5.Location = new Point(520, 0);
+            pictureBox5.Location = new Point(520, 27);
             pictureBox5.Margin = new Padding(0);
             pictureBox5.Name = "pictureBox5";
             pictureBox5.Size = new Size(104, 110);
@@ -118,7 +128,7 @@
             // pictureBox6
             // 
             pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
-            pictureBox6.Location = new Point(624, 0);
+            pictureBox6.Location = new Point(624, 27);
             pictureBox6.Margin = new Padding(0);
             pictureBox6.Name = "pictureBox6";
             pictureBox6.Size = new Size(104, 110);
@@ -129,7 +139,7 @@
             // pictureBox7
             // 
             pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
-            pictureBox7.Location = new Point(728, 0);
+            pictureBox7.Location = new Point(728, 27);
             pictureBox7.Margin = new Padding(0);
             pictureBox7.Name = "pictureBox7";
             pictureBox7.Size = new Size(104, 110);
@@ -140,7 +150,7 @@
             // pictureBox8
             // 
             pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
-            pictureBox8.Location = new Point(832, 0);
+            pictureBox8.Location = new Point(832, 27);
             pictureBox8.Margin = new Padding(0);
             pictureBox8.Name = "pictureBox8";
             pictureBox8.Size = new Size(104, 110);
@@ -148,11 +158,82 @@
             pictureBox8.TabIndex = 8;
             pictureBox8.TabStop = false;
             // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { менюToolStripMenuItem, информацияToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(936, 25);
+            menuStrip.TabIndex = 9;
+            menuStrip.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            менюToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { начатьСначалаToolStripMenuItem, выходToolStripMenuItem });
+            менюToolStripMenuItem.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            менюToolStripMenuItem.Size = new Size(57, 21);
+            менюToolStripMenuItem.Text = "Меню";
+            // 
+            // начатьСначалаToolStripMenuItem
+            // 
+            начатьСначалаToolStripMenuItem.Name = "начатьСначалаToolStripMenuItem";
+            начатьСначалаToolStripMenuItem.Size = new Size(180, 22);
+            начатьСначалаToolStripMenuItem.Text = "Начать сначала";
+            // 
+            // выходToolStripMenuItem
+            // 
+            выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            выходToolStripMenuItem.Size = new Size(180, 22);
+            выходToolStripMenuItem.Text = "Выход";
+            // 
+            // информацияToolStripMenuItem
+            // 
+            информацияToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { правилаИгрыToolStripMenuItem, оToolStripMenuItem });
+            информацияToolStripMenuItem.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            информацияToolStripMenuItem.Name = "информацияToolStripMenuItem";
+            информацияToolStripMenuItem.Size = new Size(99, 21);
+            информацияToolStripMenuItem.Text = "Информация";
+            // 
+            // правилаИгрыToolStripMenuItem
+            // 
+            правилаИгрыToolStripMenuItem.Name = "правилаИгрыToolStripMenuItem";
+            правилаИгрыToolStripMenuItem.Size = new Size(180, 22);
+            правилаИгрыToolStripMenuItem.Text = "Правила игры";
+            // 
+            // оToolStripMenuItem
+            // 
+            оToolStripMenuItem.Name = "оToolStripMenuItem";
+            оToolStripMenuItem.Size = new Size(180, 22);
+            оToolStripMenuItem.Text = "О программе";
+            // 
+            // NumberMovesLabelText
+            // 
+            NumberMovesLabelText.AutoSize = true;
+            NumberMovesLabelText.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            NumberMovesLabelText.Location = new Point(12, 177);
+            NumberMovesLabelText.Name = "NumberMovesLabelText";
+            NumberMovesLabelText.Size = new Size(189, 25);
+            NumberMovesLabelText.TabIndex = 10;
+            NumberMovesLabelText.Text = "Количество ходов:";
+            // 
+            // NumberMovesLabel
+            // 
+            NumberMovesLabel.AutoSize = true;
+            NumberMovesLabel.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            NumberMovesLabel.Location = new Point(207, 177);
+            NumberMovesLabel.Name = "NumberMovesLabel";
+            NumberMovesLabel.Size = new Size(34, 25);
+            NumberMovesLabel.TabIndex = 11;
+            NumberMovesLabel.Text = "00";
+            // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(936, 261);
+            ClientSize = new Size(936, 211);
+            Controls.Add(NumberMovesLabel);
+            Controls.Add(NumberMovesLabelText);
             Controls.Add(pictureBox8);
             Controls.Add(pictureBox7);
             Controls.Add(pictureBox6);
@@ -162,6 +243,9 @@
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Controls.Add(frogLeftPictureBox1);
+            Controls.Add(menuStrip);
+            Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            MainMenuStrip = menuStrip;
             MaximizeBox = false;
             Name = "MainForm";
             Text = "Лягушки на болоте";
@@ -174,6 +258,8 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -189,5 +275,14 @@
         private PictureBox pictureBox6;
         private PictureBox pictureBox7;
         private PictureBox pictureBox8;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem менюToolStripMenuItem;
+        private ToolStripMenuItem начатьСначалаToolStripMenuItem;
+        private ToolStripMenuItem выходToolStripMenuItem;
+        private ToolStripMenuItem информацияToolStripMenuItem;
+        private ToolStripMenuItem правилаИгрыToolStripMenuItem;
+        private ToolStripMenuItem оToolStripMenuItem;
+        private Label NumberMovesLabelText;
+        private Label NumberMovesLabel;
     }
 }
